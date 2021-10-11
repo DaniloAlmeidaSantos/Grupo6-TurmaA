28791ef6a626edb6d6972b6c760e41ab0e500d82
Danilo Almeida <48030619+DaniloAlmeidaSantos@users.noreply.github.com>
2021-10-10 21:22:57 -0300

    Initialize JAVA
    -> Add .JAVA (class)
    -> Add Project

---
+++ src/awtreech/G6.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package awtreech;
+
+/**
+ * @author's Danilo, Fernando && Guilherme
+ *
+ */
+public class G6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+}
