0e5eadd0c8e5b3efecca3a33fe7356b3f914a3c3
Danilo Almeida <48030619+DaniloAlmeidaSantos@users.noreply.github.com>
2021-10-13 12:26:42 -0300

    Melhoria nos métodos e criação de lógica para aleatoriadade
    -> Implementado Collections para o método nível médio
    -> Implementado Try Catch para tratar excessões

--- src/awtreech/G6.java
+++ src/awtreech/G6.java
@@ -4,6 +4,7 @@
 package awtreech;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /**
@@ -18,9 +19,9 @@ public class G6 {
 	Scanner input = new Scanner(System.in);
 
 	// Var Global
-	boolean push = false; // Push : True = Encerra o jogo, False = Não encerra o jogo
-	String qQuestions[] = {"a", "b", "c", "d", "e"}; // Alternativas disponíveis no jogo
-	ArrayList<String> qResponses = new ArrayList<>();
+	private boolean push = false; // Push : True = Encerra o jogo, False = Não encerra o jogo
+	private String qQuestions[] = {"a", "b", "c", "d", "e"}; // Alternativas disponíveis no jogo
+	private ArrayList<String> qResponses = new ArrayList<>();
 
 	/**
      * Foi determinado nos cases de 1 a 3 o total de vidas extras
@@ -29,30 +30,32 @@ public class G6 {
      * @param selectLevel : definir nível escolhido pelo usuário
      */
     public void level(int selectLevel) {
+		try {
+			switch(selectLevel){
 
-
-        switch(selectLevel){
-
-            case 1:
-                System.out.println("Nivel easy selecionado, Voce tem 3 vidas extras");
-                principal.easy(1, push, 3);
-                break;
-
-            case 2:
-                System.out.println("Nivel medium selecionado, Voce tem 2 vidas extras");
-                principal.medium(1, push, 2);
-                break;
-
-            case 3:
-                System.out.println("Nivel medium selecionado, Voce tem 2 vidas extras");
-                principal.hard(1, push, 1);
-                break;
-
-            default:
-                System.out.println("Nivel Invalido");
-                principal.level(3);
-                break;
-        }
+				case 1:
+					System.out.println("Nivel easy selecionado, Voce tem 3 vidas extras");
+					principal.easy(1, this.push, 3);
+					break;
+	
+				case 2:
+					System.out.println("Nivel medium selecionado, Voce tem 2 vidas extras");
+					principal.medium(1, this.push, 2, false);
+					break;
+	
+				case 3:
+					System.out.println("Nivel medium selecionado, Voce tem 2 vidas extras");
+					principal.hard(1, this.push, 1);
+					break;
+	
+				default:
+					System.out.println("Nivel Invalido");
+					principal.level(3);
+					break;
+			}
+		} catch (Exception e) {
+			System.err.println(e);
+		}
     }
 
 	/**
@@ -60,6 +63,13 @@ public class G6 {
 	 */
 	public void exit() {
 		System.exit(0);
+	}
+
+	/**
+	 * 
+	 */
+	public void instructions() {
+		
 	}
 
 	/**
@@ -73,46 +83,48 @@ public class G6 {
 	 * @param nQuestion : Número da questão
 	 */
 	public void gateway(String data, int eLife,  String cQuestion, int level, int nQuestion) {
-		// Caso o jogador decida sair do jogo
-		if (data.toLowerCase().equals("sair")) principal.exit();
-			
-		if (data.toLowerCase().equals(cQuestion)) {
-			nQuestion++;
-			switch (level) {
-				case 1: // Histórias e desafios - Fácil
-					System.out.println("Colocar fácil aqui");
-					break;
+		try {
+			// Caso o jogador decida sair do jogo
+			if (data.toLowerCase().equals("sair")) principal.exit();
+				
+			if (data.toLowerCase().equals(cQuestion)) {
+				nQuestion++;
+				switch (level) {
+					case 1: // Histórias e desafios - Fácil
+						System.out.println("Colocar fácil aqui");
+						break;
 
-				case 2: // Histórias e desafios - Médio
-					System.out.println("Resposta correta");
-					principal.medium(nQuestion, push, eLife);
-					break;
-					
-				case 3: // Histórias e desafios - Difícil
-					System.out.println("Colocar dificil aqui");
-					break;
-			}
-		} else {
-			System.out.println("Resposta incorreta");
-			eLife--;
-			switch (level) {
-				case 1: // Histórias e desafios - Fácil
-					System.out.println("Colocar fácil aqui");
-					break;
+					case 2: // Histórias e desafios - Médio
+						System.out.println("Resposta correta");
+						principal.medium(nQuestion, this.push, eLife, false);
+						break;
+						
+					case 3: // Histórias e desafios - Difícil
+						System.out.println("Colocar dificil aqui");
+						break;
+				}
+			} else {
+				System.out.println("Resposta incorreta");
+				eLife--;
+				switch (level) {
+					case 1: // Histórias e desafios - Fácil
+						System.out.println("Colocar fácil aqui");
+						break;
 
-				case 2: // Histórias e desafios - Médio
-										// Caso o player exceda o número de vidas o jogo finalizado
-					if (eLife < 0) principal.medium(nQuestion, push = true, eLife);
-					// Caso o jogador erre, mas tenha mais vidas extras ele pode responder novamente
-					else principal.medium(nQuestion, push = false, eLife); 
-					break;
-					
-				case 3: // Histórias e desafios - Difícil
-					System.out.println("Colocar dificil aqui");
-					break;
-			}
+					case 2: // Histórias e desafios - Médio
+						if (eLife < 0) principal.medium(nQuestion, this.push = true, eLife, true);
+						// Caso o jogador erre, mas tenha mais vidas extras ele pode responder novamente
+						else principal.medium(nQuestion, this.push, eLife, true); 
+						break;
+						
+					case 3: // Histórias e desafios - Difícil
+						System.out.println("Colocar dificil aqui");
+						break;
+				}
+			}	
+		} catch (Exception e) {
+			System.err.println(e);
 		}
-		
 	}
 
 	/**
@@ -176,51 +188,64 @@ public class G6 {
 	 * @param nQuestion : número de questões restantes
 	 * @param noPush : Verifica se faz push da função de história negativa
 	 * @param extraLife : Vidas extras do player
+	 * @param reloadd : Valida se essa execução é pertinente de erro ou não
 	 */
-	public void medium(int nQuestion, boolean noPush, int extraLife) {
+	public void medium(int nQuestion, boolean noPush, int extraLife, Boolean reload) throws Exception {
 		
-		String alternativa;	
+		try {
+			String alternativa;	
+			int index = 0;
 
-		if (noPush) {
-			// Saída - Histórias negativas
-			System.out.println("Você perdeu");
-		} else {
-			// Saída histórias positivas
-			switch (nQuestion) {
-				case 1:
-					// Chamar push da história positiva
-					qResponses.add("101010");
-					qResponses.add("101011");
-					qResponses.add("111010");
-					qResponses.add("101110");
-					qResponses.add("101111");
+			if (noPush) {
+				// Saída - Histórias negativas
+			} else {
+				// Saída histórias positivas
+				switch (nQuestion) {
+					case 1:
+						qResponses.clear();
+						
+						// Chamar push da história positiva
+						qResponses.add("101010"); // resposta correta
+						qResponses.add("101011");
+						qResponses.add("111010");
+						qResponses.add("101110");
+						qResponses.add("101111");
 
-					System.out.println("O valor em binário do decimal 42 é:");
-					System.out.println(qQuestions[0] + ") " + qResponses.get(0)); // resposta correta
-					System.out.println(qQuestions[1] + ") " + qResponses.get(1));
-					System.out.println(qQuestions[2] + ") " + qResponses.get(2));
-					System.out.println(qQuestions[3] + ") " + qResponses.get(3));
-					System.out.println(qQuestions[4] + ") " + qResponses.get(4));
-					System.out.print("Escolha uma alternativa: ");
+						Collections.shuffle(qResponses);
 
-					alternativa = input.next();	
-					principal.gateway(alternativa, extraLife, qQuestions[0], 2, nQuestion); // Chamando o gateway
-					break;
+						System.out.println("O valor em binário do decimal 42 é:");
+						System.out.println(qQuestions[0] + ") " + qResponses.get(0)); 
+						System.out.println(qQuestions[1] + ") " + qResponses.get(1));
+						System.out.println(qQuestions[2] + ") " + qResponses.get(2));
+						System.out.println(qQuestions[3] + ") " + qResponses.get(3));
+						System.out.println(qQuestions[4] + ") " + qResponses.get(4));
+						System.out.print("Escolha uma alternativa: ");
 
-				case 2:			
-					break;
-				case 3:			
-					break;
-				case 4:				
-					break;
-				case 5:			
-					break;
-				case 6:				
-					break;
-				case 7:
+						for (int i = 0; i < 5; i++) {
+							if (qResponses.get(i).equals("101010"))	index = i;
+						}
 
-					break;
+						alternativa = input.next();	
+						principal.gateway(alternativa, extraLife, qQuestions[index], 2, nQuestion); // Chamando o gateway
+						break;
+
+					case 2:			
+						break;
+					case 3:			
+						break;
+					case 4:				
+						break;
+					case 5:			
+						break;
+					case 6:				
+						break;
+					case 7:
+
+						break;
+				}
 			}
+		} catch (Exception e) {
+			System.err.println(e);
 		}
 	}
 
@@ -276,6 +301,13 @@ public class G6 {
 					break;
 			}
 		}
+	}
+
+	/**
+	 * 
+	 */
+	public void stories() {
+
 	}
 
 	/**
